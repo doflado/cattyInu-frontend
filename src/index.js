39f8f7b91a803398d76c7604cab237fb473f575e
yUradog <kyotomo920@gmail.com>
2024-02-29 01:49:01 -0500

    update issues for vercel deploy

--- src/index.js
+++ src/index.js
@@ -4,41 +4,10 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-import "@rainbow-me/rainbowkit/styles.css";
-import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { mainnet, base, bsc, zkSync, bscTestnet } from "wagmi/chains";
-import { alchemyProvider } from "wagmi/providers/alchemy";
-import { publicProvider } from "wagmi/providers/public";
-
-const { chains, publicClient } = configureChains(
-  [bscTestnet, mainnet, bsc],
-  [
-    alchemyProvider({ apiKey: "ekZhZsGjfWuK39pYW_YXSEcRKDN8amSN" }),
-    publicProvider(),
-  ]
-);
-
-const { connectors } = getDefaultWallets({
-  appName: "walletConnect",
-  projectId: "fe62b424c4ab666f47d64744e0b3dca0",
-  chains,
-});
-
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient,
-});
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains}>
-        <App />
-      </RainbowKitProvider>
-    </WagmiConfig>
+    <App />
   </React.StrictMode>
 );
 
